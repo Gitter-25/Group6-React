e99119d4df62c0b2ffee2c16f25b0db042e7875d
Gitter-25 <johngitterperitol@gmail.com>
2026-04-20 22:15:31 +0800

    Activity2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/home/Home"
-
+import Timer from "./pages/activities/activity2/Timer"
 
 
 const App = () => {
@@ -9,9 +9,10 @@ const App = () => {
   
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
+      {/* <Route path="/" element={<Home />} /> */}
       
       <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Timer />} />
     </Routes>
   
   </BrowserRouter>
